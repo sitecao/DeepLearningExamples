f95070326dc757c32f13e84fb9adc9c742cb1dce
Ubuntu <ubuntu@ip-192-168-78-215.ec2.internal>
2020-06-29 17:35:09 +0000

    add herring support to training

--- PyTorch/Segmentation/MaskRCNN/pytorch/maskrcnn_benchmark/engine/trainer.py
+++ PyTorch/Segmentation/MaskRCNN/pytorch/maskrcnn_benchmark/engine/trainer.py
@@ -6,9 +6,11 @@ import time
 
 import torch
 import torch.distributed as dist
-
-from maskrcnn_benchmark.utils.comm import get_world_size
+import herring.torch as herring
+#from maskrcnn_benchmark.utils.comm import get_world_size
 from maskrcnn_benchmark.utils.metric_logger import MetricLogger
+
+from herring.torch import get_world_size
 
 try:
     from apex import amp
@@ -33,11 +35,12 @@ def reduce_loss_dict(loss_dict):
             loss_names.append(k)
             all_losses.append(loss_dict[k])
         all_losses = torch.stack(all_losses, dim=0)
-        dist.reduce(all_losses, dst=0)
-        if dist.get_rank() == 0:
+        #dist.reduce(all_losses, dst=0)
+        herring.all_reduce(all_losses)
+        #if herring.get_rank() == 0:
             # only main process gets accumulated, so only divide by
             # world_size in this case
-            all_losses /= world_size
+        all_losses /= world_size
         reduced_losses = {k: v for k, v in zip(loss_names, all_losses)}
     return reduced_losses
 
@@ -53,17 +56,19 @@ def do_train(
     arguments,
     use_amp,
     cfg,
-    dllogger,
     per_iter_end_callback_fn=None,
 ):
-    dllogger.log(step="PARAMETER", data={"train_start": True})
+    logger = logging.getLogger("maskrcnn_benchmark.trainer")
+    logger.info("Start training")
     meters = MetricLogger(delimiter="  ")
     max_iter = len(data_loader)
+    print("max_iter: ", max_iter)
     start_iter = arguments["iteration"]
     model.train()
     start_training_time = time.time()
     end = time.time()
     for iteration, (images, targets, _) in enumerate(data_loader, start_iter):
+        print("iter", iteration)
         data_time = time.time() - end
         iteration = iteration + 1
         arguments["iteration"] = iteration
@@ -72,13 +77,13 @@ def do_train(
         targets = [target.to(device) for target in targets]
 
         loss_dict = model(images, targets)
-
+        print("finish forward")
         losses = sum(loss for loss in loss_dict.values())
 
         # reduce losses over all GPUs for logging purposes
-        loss_dict_reduced = reduce_loss_dict(loss_dict)
-        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-        meters.update(loss=losses_reduced, **loss_dict_reduced)
+        #loss_dict_reduced = reduce_loss_dict(loss_dict)
+        #losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+        meters.update(loss=losses, **loss_dict)
 
 
         # Note: If mixed precision is not used, this ends up doing nothing
@@ -88,7 +93,7 @@ def do_train(
                 scaled_losses.backward()
         else:
             losses.backward()
-
+        print("finish backward")
         if not cfg.SOLVER.ACCUMULATE_GRAD:
             optimizer.step()
             scheduler.step()
@@ -101,7 +106,7 @@ def do_train(
                 optimizer.step()
                 scheduler.step()
                 optimizer.zero_grad()
-            
+        print("finish update weigiht")
         batch_time = time.time() - end
         end = time.time()
         meters.update(time=batch_time, data=data_time)
@@ -110,16 +115,31 @@ def do_train(
         eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
 
         if iteration % 20 == 0 or iteration == max_iter:
-            log_data = {"eta":eta_string, "learning_rate":optimizer.param_groups[0]["lr"],
-                        "memory": torch.cuda.max_memory_allocated() / 1024.0 / 1024.0 }
-            log_data.update(meters.get_dict())
-            dllogger.log(step=(iteration,), data=log_data)
-
-        if cfg.SAVE_CHECKPOINT:
-            if iteration % checkpoint_period == 0:
-                checkpointer.save("model_{:07d}".format(iteration), **arguments)
-            if iteration == max_iter:
-                checkpointer.save("model_final", **arguments)
+            logger.info(
+                meters.delimiter.join(
+                    [
+                        "eta: {eta}",
+                        "avg iteration time(s): {avg_iter:.2f}",
+                        "avg iter/s: {iter_s:.2f}",
+                        "iter: {iter}",
+                        "{meters}",
+                        "lr: {lr:.6f}",
+                        "max mem: {memory:.0f}",
+                    ]
+                ).format(
+                    eta=eta_string,
+                    avg_iter=meters.time.global_avg,
+                    iter_s = 1.0/meters.time.global_avg,
+                    iter=iteration,
+                    meters=str(meters),
+                    lr=optimizer.param_groups[0]["lr"],
+                    memory=torch.cuda.max_memory_allocated() / 1024.0 / 1024.0,
+                )
+            )
+        if iteration % checkpoint_period == 0:
+            checkpointer.save("model_{:07d}".format(iteration), **arguments)
+        if iteration == max_iter:
+            checkpointer.save("model_final", **arguments)
 
         # per-epoch work (testing)
         if per_iter_end_callback_fn is not None:
@@ -127,14 +147,11 @@ def do_train(
             if early_exit:
                 break
 
+
     total_training_time = time.time() - start_training_time
     total_time_str = str(datetime.timedelta(seconds=total_training_time))
-    dllogger.log(step=tuple(), data={"e2e_train_time": total_training_time,
-                                                   "train_perf_fps": max_iter * cfg.SOLVER.IMS_PER_BATCH / total_training_time})
-    logger = logging.getLogger("maskrcnn_benchmark.trainer")
     logger.info(
-    "Total training time: {} ({:.4f} s / it)".format(
-        total_time_str, total_training_time / (max_iter)
+        "Total training time: {} ({:.4f} s / it)".format(
+            total_time_str, total_training_time / (max_iter)
         )
     )
-
