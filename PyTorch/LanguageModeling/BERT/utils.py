552328d740afe1f2c5aab1a783b315d7032f7144
Piyush Ghai <ghai.8@osu.edu>
2020-08-05 14:24:13 -0700

    Converted BERT PT script to work with Herring

--- PyTorch/LanguageModeling/BERT/utils.py
+++ PyTorch/LanguageModeling/BERT/utils.py
@@ -12,21 +12,21 @@
 # limitations under the License.
 
 import torch
-import torch.distributed as dist
+import herring.torch as herring
 
 def get_rank():
-    if not dist.is_available():
-        return 0
-    if not dist.is_initialized():
-        return 0
-    return dist.get_rank()
+    # if not dist.is_available():
+    #     return 0
+    # if not dist.is_initialized():
+    #     return 0
+    return herring.get_rank()
 
 def get_world_size():
-    if not dist.is_available():
-        return 1
-    if not dist.is_initialized():
-        return 1
-    return dist.get_world_size()
+    # if not dist.is_available():
+    #     return 1
+    # if not dist.is_initialized():
+    #     return 1
+    return herring.get_world_size()
 
 def is_main_process():
     return get_rank() == 0
