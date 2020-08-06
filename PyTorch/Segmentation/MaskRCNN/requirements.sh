d4f0896d7fdd508eabe335c6f55f41e36d99ce76
Ubuntu <ubuntu@ip-192-168-89-232.ec2.internal>
2020-08-06 17:34:18 +0000

    Added requirements.sh

---
+++ PyTorch/Segmentation/MaskRCNN/requirements.sh
@@ -0,0 +1,14 @@
+
+WORK_DIR=`mktemp -d`
+
+pip install ninja yacs cython matplotlib tqdm opencv-python
+cd $WORK_DIR
+git clone https://github.com/NVIDIA/apex; cd apex;
+python setup.py install --cuda_ext --cpp_ext
+cd $WORK_DIR
+git clone https://github.com/cocodataset/cocoapi.git; cd cocoapi/PythonAPI;
+python setup.py build_ext instal
+cd /shared/MaskRCNN/pytorch
+python setup.py build develop
+
+rm -rf $WORK_DIR
