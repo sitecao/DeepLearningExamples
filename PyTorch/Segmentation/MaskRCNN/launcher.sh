d25684fff0596de63994c15ae0c7b40cb124d059
Ubuntu <ubuntu@ip-192-168-89-232.ec2.internal>
2020-08-06 17:33:31 +0000

    Added launcher.sh

---
+++ PyTorch/Segmentation/MaskRCNN/launcher.sh
@@ -0,0 +1,2 @@
+
+herringrun python /shared/MaskRCNN/pytorch/train_net_herring.py --config-file /shared/MaskRCNN/pytorch/configs/e2e_mask_rcnn_R_50_FPN_1x_64GPU_4bs.yaml --skip-test DTYPE "float16" OUTPUT_DIR /shared/results
