28d8e659274c6d4812e1c24e194370e9b74a9b6e
Indu <thangakr@amazon.com>
2020-11-03 19:33:13 +0000

    Record mask rcnn throughput starting from batch 500

--- PyTorch/Segmentation/MaskRCNN/pytorch/maskrcnn_benchmark/engine/trainer.py
+++ PyTorch/Segmentation/MaskRCNN/pytorch/maskrcnn_benchmark/engine/trainer.py
@@ -107,33 +107,39 @@ def do_train(
                 optimizer.zero_grad()
         batch_time = time.time() - end
         end = time.time()
-        meters.update(time=batch_time, data=data_time)
 
-        eta_seconds = meters.time.global_avg * (max_iter - iteration)
-        eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+        if iteration % 20 == 0:
+            logger.info("iter: %d batch_time: %f" % (iteration, batch_time))
 
-        if iteration % 20 == 0 or iteration == max_iter:
-            logger.info(
-                meters.delimiter.join(
-                    [
-                        "eta: {eta}",
-                        "avg iteration time(s): {avg_iter:.2f}",
-                        "avg iter/s: {iter_s:.2f}",
-                        "iter: {iter}",
-                        "{meters}",
-                        "lr: {lr:.6f}",
-                        "max mem: {memory:.0f}",
-                    ]
-                ).format(
-                    eta=eta_string,
-                    avg_iter=meters.time.global_avg,
-                    iter_s = 1.0/meters.time.global_avg,
-                    iter=iteration,
-                    meters=str(meters),
-                    lr=optimizer.param_groups[0]["lr"],
-                    memory=torch.cuda.max_memory_allocated() / 1024.0 / 1024.0,
+        if(iteration > 500):
+            meters.update(time=batch_time, data=data_time)
+
+            eta_seconds = meters.time.global_avg * (max_iter - iteration)
+            eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+
+            if iteration % 20 == 0 or iteration == max_iter:
+                logger.info(
+                    meters.delimiter.join(
+                        [
+                            "eta: {eta}",
+                            "avg iteration time(s): {avg_iter:.2f}",
+                            "avg iter/s: {iter_s:.2f}",
+                            "iter: {iter}",
+                            "{meters}",
+                            "lr: {lr:.6f}",
+                            "max mem: {memory:.0f}",
+                        ]
+                    ).format(
+                        eta=eta_string,
+                        avg_iter=meters.time.global_avg,
+                        iter_s = 1.0/meters.time.global_avg,
+                        iter=iteration,
+                        meters=str(meters),
+                        lr=optimizer.param_groups[0]["lr"],
+                        memory=torch.cuda.max_memory_allocated() / 1024.0 / 1024.0,
+                    )
                 )
-            )
+
         if iteration % checkpoint_period == 0:
             checkpointer.save("model_{:07d}".format(iteration), **arguments)
         if iteration == max_iter:
