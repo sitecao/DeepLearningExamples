6df4c837459aee6905fa98cbbb5f15e760bde6d4
Carolyn Wang <carowang@amazon.com>
2021-11-01 04:34:35 +0000

    remove apex from requirements

--- PyTorch/Segmentation/MaskRCNN/requirements.sh
+++ PyTorch/Segmentation/MaskRCNN/requirements.sh
@@ -5,13 +5,13 @@ pip install ninja yacs cython matplotlib tqdm opencv-python pybind11==2.5.0 'git
 
 pip --no-cache-dir --no-cache install 'git+https://github.com/NVIDIA/cocoapi#egg=pycocotools&subdirectory=PythonAPI'
 
-cd $WORK_DIR
-git clone https://github.com/NVIDIA/apex; cd apex;
-python setup.py install --cuda_ext --cpp_ext
+#cd $WORK_DIR
+#git clone https://github.com/NVIDIA/apex; cd apex;
+#python setup.py install --cuda_ext --cpp_ext
 
 cd /shared/DeepLearningExamples/PyTorch/Segmentation/MaskRCNN/pytorch/
 python setup.py build develop
 
 wget https://dl.fbaipublicfiles.com/detectron/ImageNetPretrained/MSRA/R-50.pkl -P /root/.torch/models/
 
-rm -rf $WORK_DIR
+#rm -rf $WORK_DIR
